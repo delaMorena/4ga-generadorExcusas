f06704e23c51bb0cf5526f9bd8b4ce4bbaa3acd5
delaMorena <martadmdo@gmail.com>
2020-09-29 10:37:54 +0000

    terminado, con los estilos y los link

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-require('./style.css');
-require('bootstrap/dist/js/bootstrap.js')
-require('bootstrap/dist/css/bootstrap.css')
-function generadorExcusas () {
+window.onload = function() { 
     let quien = ["Mi madre","Mi abuela", "La policía", "Un extraterreste", "Simbad el marino", "Un señor de Murcia", "La del quinto", "El perro", "Un ratón", "Un bombero"]
     let accion = ["se comió", "pisó" ,"tiró al WC", "tiró a la basura" ,"robó","quemó","manchó", "mojó", "trituró", "se hizo popó en"]
     let que = ["mis deberes","mis cuadernos","mi tarea","mi mochila","nuestra casa", "mi estuche"]
@@ -10,17 +7,6 @@ function generadorExcusas () {
     let posicionAccion = [Math.floor(Math.random() * 10)];
     let posicionQue = [Math.floor(Math.random() * 6)];
     let posicionCuando = [Math.floor(Math.random() * 6)];
-
-    Let myExcuse = quien[posicionQuien]+" "+accion[posicionAccion]+" "+que[posicionQue]+" "+cuando[posicionCuando]
-
-    return myExcuse
-
-    window.onload = function myExcuse() {
-            console.log ("Lo siento señorita, no pude hacer lo que pidió porque"+ myExcuse);
-        }
-    
-    
-}
-//let excuse = whoExcuse+" "+actionExcuse+" "+whatExcuse+" "+whenExcuse;
-//    document.getElementById("generator").innerHTML = excuse;
-//   // console.log(excuse);
+    let myExcuse = "¡"+ quien[posicionQuien]+" "+accion[posicionAccion]+" "+que[posicionQue]+" "+cuando[posicionCuando]+"!";
+        document.getElementById("generator").innerHTML = myExcuse;         
+}    
